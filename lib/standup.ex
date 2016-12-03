d600d973a99cdd96c51e2a9c6412e099a8208ed4
Marat Galiev <kazanlug@gmail.com>
2016-12-03 23:51:51 +0300

    Basic notification for standups

--- lib/standup.ex
+++ lib/standup.ex
@@ -12,10 +12,17 @@ defmodule Standup do
       # Starts a worker by calling: Standup.Worker.start_link(arg1, arg2, arg3)
       # worker(Standup.Worker, [arg1, arg2, arg3]),
     ]
-
+    add_jobs()
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Standup.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def add_jobs do
+    Quantum.add_job(:standup, %Quantum.Job{
+      schedule: Application.get_env(:standup, Standup.Responders.Standup)[:time_of_day],
+      task: fn -> Standup.Responders.Standup.run end
+    })
   end
 end
